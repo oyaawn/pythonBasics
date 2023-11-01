8310a317f53407ea1500f5f7d996c372e4611d76
oyaawn <ayan.chatterjee@mailbox.tu-dresden.de>
2023-11-01 17:27:04 +0100

    hello main

---
+++ pythonBasics-master/ifElse.py
@@ -0,0 +1,18 @@
+""" nums= [1,2,3,4,5,6,7,8,9,10
+for num in nums:
+    print(num, 'is even') if(num%2==0) else print(num, 'is odd')
+    
+print('Done')
+
+
+
+
+
+while(x<10):
+    print(x
+ """
+x=0
+
+while True:
+    print(x)
+    x+=1;
