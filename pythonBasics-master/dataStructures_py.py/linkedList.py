9a3e2bb34afdf82ae47ffc27dac8070d3738ffdf
oyaawn <ayan.chatterjee@mailbox.tu-dresden.de>
2023-11-04 22:09:32 +0100

    Add linkedList wrapper class to node class in linkedList.py

--- pythonBasics-master/dataStructures_py.py/linkedList.py
+++ pythonBasics-master/dataStructures_py.py/linkedList.py
@@ -1,6 +1,8 @@
-class node:
+class node: # node class
     def __init__(self,data=None):
         self.data = data
         self.next = None
 
-        
+class linkedList: # linked list wrapper class
+    def __init__(self):
+        self.head = node()
