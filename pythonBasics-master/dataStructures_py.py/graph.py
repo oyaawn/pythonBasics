d35ffdd9b63e42a31298221ad7f08e7f41475f01
oyaawn <ayan.chatterjee@mailbox.tu-dresden.de>
2023-11-06 03:15:00 +0100

    graph g

---
+++ pythonBasics-master/dataStructures_py.py/graph.py
@@ -0,0 +1,33 @@
+class Graph:
+    def __init__(self, node1, node2):
+        self.graph={}
+
+    def addNode(self,node):
+        self.graph[node]={}
+
+    def addEdgeNoWeight(self,node1,node2):
+        if node1 not in self.graph:
+            self.graph[node1]=[node2]
+        else:
+            self.graph[node1].append(node2)
+
+        if node2 not in self.graph:
+            self.graph[node2]=[node1]
+        else:
+            self.graph[node2].append(node1)
+
+    def addEdgeWeight(self,node1,node2,weight):
+        if node1 not in self.graph:
+            self.graph[node1]={node2:weight}
+        else:
+            self.graph[node1][node2]=weight
+
+        if node2 not in self.graph:
+            self.graph[node2]={node1:weight}
+        else:
+            self.graph[node2][node1]=weight
+
+    def printGraph(self):
+        for node in self.graph:
+            print(node, ":", self.graph[node])
+            
